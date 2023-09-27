82c1aa7bb777831d9c445fe97645b34edd2094fb
Yassir Taousi <46757421+ytaousi@users.noreply.github.com>
2023-09-27 04:04:19 +0100

    Create spider.py

---
+++ spider.py
@@ -0,0 +1,17 @@
+def extract_urls():
+    print("extracting urls")
+
+
+
+def extract_images():
+    print("extracting images")
+
+
+
+def download_images():
+    print("downloading images")
+
+
+
+def spidey_scrap():
+    print("this is the main function")
