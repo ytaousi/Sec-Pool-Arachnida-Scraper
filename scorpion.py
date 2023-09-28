1ca3c7917daab03fda8c43a485551c1749b44e46
ytaousi <yassir.taous@gmail.com>
2023-09-28 09:54:43 -0400

    ... scorpion done ?

--- scorpion.py
+++ scorpion.py
@@ -1,2 +1,21 @@
-def scorpion():
-    print("Metatata")
+import sys
+import exiftool
+
+
+extentions = ["jpg", "jpeg", "png", "gif", "bmp"]
+files = sys.argv[1:]
+
+
+def print_metadata(file):
+    with exiftool.ExifToolHelper() as ext:
+        metadata = ext.get_metadata(file)
+        for line in metadata:
+            for key, value in line.items():
+                print(key.split(":")[-1], value)  
+
+for file in files:
+    if file.split(".")[-1] in extentions:
+        metadata = print_metadata(file)
+        print("****************************************")
+    else:
+        print("error extracting metadata")
